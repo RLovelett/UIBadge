2409a16222a6f9cced7cb4122cfdd388a418a190
Ryan Lovelett <ryan.a.lovelett@saic.com>
2015-08-28 19:33:34 -0400

    Initial test

---
+++ UIBadge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIBadge
+//
+//  Created by Ryan Lovelett on 8/28/15.
+//  Copyright © 2015 SAIC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
